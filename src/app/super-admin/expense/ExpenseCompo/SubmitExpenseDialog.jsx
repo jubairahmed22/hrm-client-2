b292e3c235840fc3e2cd05d31526c83029b9ebe9
jubairahmed22 <jubairahmed060@gmail.com>
2026-04-25 19:58:33 +0600

    something is added

--- src/app/super-admin/expense/ExpenseCompo/SubmitExpenseDialog.jsx
+++ src/app/super-admin/expense/ExpenseCompo/SubmitExpenseDialog.jsx
@@ -147,7 +147,7 @@ const SubmitExpenseDialog = ({ isOpen, onClose, categories = [] }) => {
                     value={String(cat._id)} 
                     className="cursor-pointer py-3"
                   >
-                    <span >{cat.categoryName}</span>af
+                    <span >{cat.categoryName}</span>
                   </SelectItem>
                 ))}
               </SelectContent>
