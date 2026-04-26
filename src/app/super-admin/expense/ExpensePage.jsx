afae36c3ccd35c51c4ce55fe44de33b94720f80d
jubairahmed22 <jubairahmed060@gmail.com>
2026-04-26 17:21:35 +0600

    something is added

--- src/app/super-admin/expense/ExpensePage.jsx
+++ src/app/super-admin/expense/ExpensePage.jsx
@@ -95,7 +95,7 @@ const ExpensePage = () => {
   );
 
   // --- VIEW: Policies ---
-  const RenderPolicies = () => <PoliciesTab></PoliciesTab>; wadfas
+  const RenderPolicies = () => <PoliciesTab></PoliciesTab>;
 
   // --- VIEW: Analytics ---
   const RenderAnalytics = () => <AnalyticsTab></AnalyticsTab>;
