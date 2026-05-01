aaaf79bd10497038d916aa99c74c20c3fa57c8a3
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-02 01:07:26 +0600

    add new modify in employee page attandenace

--- src/app/employee/attendance/AttendancePage.jsx
+++ src/app/employee/attendance/AttendancePage.jsx
@@ -58,10 +58,10 @@ const AttendancePage = () => {
       {/* ================= TABS ================= */}
       <Tabs value={activeTab} onValueChange={handleTabChange} className="space-y-6">
 
-        <TabsList className="grid w-full grid-cols-3">
+        <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="today">Today</TabsTrigger>
           <TabsTrigger value="attendance">My Attendance</TabsTrigger>
-          <TabsTrigger value="allEmployees">All Employees Attendance</TabsTrigger>
+          {/* <TabsTrigger value="allEmployees">All Employees Attendance</TabsTrigger> */}
           {/* <TabsTrigger value="leave">Leave</TabsTrigger>
           <TabsTrigger value="salary">Salary</TabsTrigger>
           <TabsTrigger value="performance">Performance</TabsTrigger> */}
