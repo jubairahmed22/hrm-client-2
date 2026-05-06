cf968b5e3cd53d8da5698fc464fc3df525d251e1
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-06 21:27:14 +0600

    added something

--- src/app/super-admin/recruitment-nextzen/page.jsx
+++ src/app/super-admin/recruitment-nextzen/page.jsx
@@ -4,7 +4,7 @@ import Tablisted from './RecNextCompo/Tablisted';
 
 const page = () => {
     return (
-        <div className='p-6  space-y-8'>
+        <div className='p-6  space-y-8'> wrq
              <JobPostHeader></JobPostHeader>
              <Tablisted></Tablisted>
         </div>
