ba3607d6dca3e56d208c9862ffce1ec46b32d376
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-06 21:28:21 +0600

    added something

--- src/app/super-admin/recruitment-nextzen/page.jsx
+++ src/app/super-admin/recruitment-nextzen/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
     return (
         <div className='p-6  space-y-8'>
              <JobPostHeader></JobPostHeader>
-             <Tablisted></Tablisted>
+             <Tablisted></Tablisted>aewrq
         </div>
     );
 };
